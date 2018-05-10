862d0a5bb275e5dac68e72d472fb8b0b6946e548
zhangbaowei <zbw911@gmail.com>
2018-05-10 16:12:08 +0800

    修改项目结构, 由试验性代码转正式

---
+++ JobClient/log/XxlJobFileAppender.cs
@@ -0,0 +1,120 @@
+﻿using ConsoleApp1.model;
+using log4net;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.log
+{
+    public class XxlJobFileAppender
+    {
+        static ILog logger = LogManager.GetLogger("xxl - job logger");
+
+        public static ThreadLocal<String> contextHolder = new ThreadLocal<String>();
+        public static String logPath = "/data/applogs/xxl-job/jobhandler/";
+
+
+        public static String makeLogFileName(DateTime triggerDate, int logId)
+        {
+            if (!Directory.Exists(logPath))
+            {
+                Directory.CreateDirectory(logPath);
+            }
+
+            String nowFormat = triggerDate.ToString("yyyy-MM-dd");
+
+            var filePathDateDir = Path.Combine(logPath, nowFormat);
+            if (!Directory.Exists(filePathDateDir))
+            {
+                Directory.CreateDirectory(filePathDateDir);
+            }
+
+            // filePath/yyyy-MM-dd/9999.log
+            String logFileName = filePathDateDir + ("/") + (logId) + ".log";
+            return logFileName;
+        }
+
+
+        /**
+	 * append log
+	 *
+	 * @param logFileName
+	 * @param appendLog
+	 */
+        public static void appendLog(String logFileName, String appendLog)
+        {
+
+            // log
+            if (appendLog == null)
+            {
+                appendLog = "";
+            }
+            appendLog += "\r\n";
+
+            // log file
+            if (logFileName == null || logFileName.Trim().Length == 0)
+            {
+                return;
+            }
+
+
+            if (!File.Exists(logFileName))
+            {
+                using (File.Create(logFileName)) { }
+            }
+
+            File.AppendAllText(logFileName, appendLog);
+
+        }
+
+        public static LogResult readLog(String logFileName, int fromLineNum)
+        {
+
+            // valid log file
+            if (logFileName == null || logFileName.Trim().Length == 0)
+            {
+                return new LogResult(fromLineNum, 0, "readLog fail, logFile not found", true);
+            }
+
+
+            if (!File.Exists(logFileName))
+            {
+                return new LogResult(fromLineNum, 0, "readLog fail, logFile not exists", true);
+            }
+
+            // read file
+            StringBuilder logContentBuffer = new StringBuilder();
+            int toLineNum = 0;
+            using (TextReader reader = File.OpenText(logFileName))
+            {
+                do
+                {
+                    string line = reader.ReadLine();
+                    if (line == null)
+                        break;
+                    if (toLineNum++ >= fromLineNum)
+                    {
+                        logContentBuffer.Append(line).Append("\n");
+                    }
+                }
+                while (true);
+            }
+
+
+            // result
+            LogResult logResult = new LogResult(fromLineNum, toLineNum, logContentBuffer.ToString(), false);
+            return logResult;
+
+            /*
+            // it will return the number of characters actually skipped
+            reader.skip(Long.MAX_VALUE);
+            int maxLineNum = reader.getLineNumber();
+            maxLineNum++;	// 最大行号
+            */
+        }
+    }
+}
---
+++ UnitTestProject1/log/XxlJobLoggerTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1.log;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.log.Tests
+{
+    [TestClass()]
+    public class XxlJobLoggerTests
+    {
+        [TestMethod()]
+        public void logTest()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                XxlJobFileAppender.contextHolder.Value = XxlJobFileAppender.makeLogFileName(System.DateTime.Now, 1);
+                XxlJobLogger.log($"{i}|aaaaaaaaaaaaaaaa");
+            }
+
+
+        }
+
+        [TestMethod()]
+        public void logTest1()
+        {
+            XxlJobFileAppender.contextHolder.Value = XxlJobFileAppender.makeLogFileName(System.DateTime.Now, 1);
+
+            var result = XxlJobFileAppender.readLog(XxlJobFileAppender.contextHolder.Value, 1);
+
+            Console.WriteLine(result.logContent);
+        }
+    }
+}
